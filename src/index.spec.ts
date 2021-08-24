360408d228d6598b69e1ac0523764cccbd972ae9
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-24 21:32:22 +0800

    add() returns 0

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,7 @@
 import { add } from "./index";
 
 describe("Test add function", () => {
-  it("should do something", () => {});
+  it("should return 0 when input is an empty string", () => {
+    expect(add("")).toEqual(0);
+  });
 });
