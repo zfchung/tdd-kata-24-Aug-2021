54c69dbf089bcc932330857227b0edd9d80282b2
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-24 21:40:37 +0800

    Extracted valueList const from numberedValueList const

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export function add(input: string): number {
   const value = input ? input : "0";
-  const numberedValueList = value.split(",").map(Number);
-  const sumValueList =
+  const valueList = value.split(",");
+  const numberedValueList = valueList.map(Number);
+  const summedValue =
     numberedValueList[0] + (numberedValueList[1] ? numberedValueList[1] : 0);
-  return sumValueList;
+  return summedValue;
 }
