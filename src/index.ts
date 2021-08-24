06e5144e32e8d36b5be41e0e0651ee194131779f
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-24 21:44:31 +0800

    Extracted sumListItems() function from add()

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ export function add(input: string): number {
   const value = input ? input : "0";
   const valueList = value.split(",");
   const numberedValueList = valueList.map(Number);
-  const summedValue = numberedValueList.reduce(
+  const summedValue = sumListItems(numberedValueList);
+  return summedValue;
+}
+function sumListItems(numberedValueList: number[]) {
+  return numberedValueList.reduce(
     (accumulator, currentValue) => accumulator + currentValue
   );
-  return summedValue;
 }
